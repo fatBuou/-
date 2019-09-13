91e937ae71b194ac9604d15613e25b8736fdc60d
fatbuou <jui368@163.com>
2019-09-13 11:23:31 +0800

    增加中秋的祝福语

--- wxrobot.py
+++ wxrobot.py
@@ -4,14 +4,15 @@ from wxpy import *
 import os
 import random
 import time
-from zhufu import Belssing
+from zhufu import spring_Festival
+from zhufu import Mid_Autumn
 
 bot = Bot(cache_path=True,qr_path='test.jpg')
 bot.enable_puid('wxpy_puid.pkl')
 my_friends = bot.friends()
 for friends in my_friends:
     try:
-        msg = Belssing.belssing[random.randint(0,35)]
+        msg = Mid_Autumn.belssing[random.randint(0,19)]
         friend = my_friends.search(friends.name)[0]
         friend.send_msg(msg=msg)
         print(friend,msg)
